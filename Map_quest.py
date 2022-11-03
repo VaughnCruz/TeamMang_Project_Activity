8414262084f142314f54fb4ab5bbd9d115c69c08
Choongqihan <qcachoong@tip.edu.ph>
2022-11-03 22:58:20 +0800

    New commit

--- Map_quest.py
+++ Map_quest.py
@@ -15,36 +15,37 @@ heading.pack()
 LabelLocation = Label(text="Enter your Current Location: ",
                       font="Courier 13", fg="#333a56", anchor='nw')
 LabelLocation.pack(pady=10)
-location = Entry(root, width=50, font=5)
-location.pack(pady=20)
+orig = Entry(root, width=50, font=5)
+orig.pack(pady=20)
 
 #Destination Output
 LabelDestination = Label(
     text="Enter your Destination Location: ", font="Courier 13", fg="#333a56")
 LabelDestination.pack(pady=10)
-destination = Entry(root, width=50, font=5)
-destination.pack(pady=15)
+dest = Entry(root, width=50, font=5)
+dest.pack(pady=15)
 
 #Funtion to clear inputs
 def clear():
-    location.delete(0,END)
-    destination.delete(0,END)
+    orig.delete(0,END)
+    dest.delete(0,END)
 
 #Funtion to display directions 
 def onClick():
     while True:
 
-        url = main_api + urllib.parse.urlencode({"key": key, "from": location, "to": destination})
+        url = main_api + urllib.parse.urlencode({"key": key, "from": orig, "to": dest})
         print("URL ", (url))
         json_data = requests.get(url).json()
         json_status = json_data["info"]["statuscode"]
         if json_status == 0:
+
             argument = "\nAPI Status: " + str(json_status) + " = A successful route call.\n"\
                 "\n================================================================================="\
-                + "\nDirections from " + (location.get()) + " to " + (destination.get())\
+                + "\nDirections from: " + (orig.get()) + " to " + (dest.get())\
                 + "\nTrip Duration: " + (json_data["route"]["formattedTime"])\
-                + "\nKilometers: " + str("{:.2f}".format(json_data["route"]["distance"] * 1.6))\
-                + "\nFuel Used (Ltr): " + str("{:.3f}".format(json_data["route"]["fuelUsed"]*3.78))\
+                + "\nKilometers: " + str("{:.2f}".format((json_data["route"]["distance"] * 1.6)))\
+                + "\nFuel Used (Ltr): " + str("{:.3f}".format((json_data["route"]["fuelUsed"]*3.78)))\
                 + "\n================================================================================="\
                 + "\nDetailed Information of travel"\
                 + "\nFrom country of: " + (json_data["route"]["locations"][0]["adminArea1"])\
@@ -64,7 +65,7 @@ def onClick():
             myResult1 = Label(root, text=argument)
             myResult1.pack()
             for each in json_data["route"]["legs"][0]["maneuvers"]:
-                argument2 = (each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)")
+                argument2 = ((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))
                 myResult2 = Label(root, text=argument2,
                                   font="Courier 9", justify=LEFT)
                 myResult2.pack()
